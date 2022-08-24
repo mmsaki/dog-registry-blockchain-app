5a5be9675ed3a7b9e55586f11d8b801624473180
mmsaki <meek10x@gmail.com>
2022-08-24 10:07:49 -0500

    [feat] added dog registry features to contract with ERC721 functionality

--- contracts/DogNFT.sol
+++ contracts/DogNFT.sol
@@ -2,33 +2,42 @@
 pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
-import "./DogRegistry.sol";
 
 contract DogNFT is ERC721Full {
     constructor() public ERC721Full("DogRegistryToken", "DOG") {}
 
+    modifier onlyPuppyOwner(address puppyOwner,uint tokenId){
+        require(ownerOf(tokenId) == msg.sender, "Only puppy token owner can call this function.");
+        require(puppyOwner == msg.sender, "You are not a registered owner of this puppy.");
+        _;
+    }
+
     struct Dog {
         string name;
         string breed;
+        string dame;
+        string sire;
         uint256 appraisalValue;
-        string dogJson;
+        string image;
     }
 
     mapping(uint256 => Dog) public dogCollection;
 
-    event Appraisal(uint256 tokenId, uint256 appraisalValue, string reportURI, string dogJson);
+    event Appraisal(uint256 tokenId, uint256 appraisalValue, string reportURI, string image);
     
     function imageUri(
         uint256 tokenId
 
-    ) public view returns (string memory imageJson){
-        return dogCollection[tokenId].dogJson;
+    ) public view returns (string memory imageURI){
+        return dogCollection[tokenId].image;
     }
 
     function registerDog(
         address owner,
         string memory name,
         string memory breed,
+        string memory dame, 
+        string memory sire,
         uint256 initialAppraisalValue,
         string memory tokenURI,
         string memory tokenJSON
@@ -38,7 +47,7 @@ contract DogNFT is ERC721Full {
         _mint(owner, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
-        dogCollection[tokenId] = Dog(name, breed, initialAppraisalValue, tokenJSON);
+        dogCollection[tokenId] = Dog(name, breed, dame, sire, initialAppraisalValue, tokenJSON);
 
         return tokenId;
     }
@@ -49,7 +58,7 @@ contract DogNFT is ERC721Full {
         string memory reportURI,
         string memory tokenJSON
         
-    ) public returns (uint256) {
+    ) public onlyPuppyOwner(msg.sender, tokenId) returns (uint256) {
         dogCollection[tokenId].appraisalValue = newAppraisalValue;
 
         emit Appraisal(tokenId, newAppraisalValue, reportURI, tokenJSON);
